4027fd13a8475c5b4689ef26047f6171902ee100
Ankit Patel <cgankit@gmail.com>
2022-03-31 16:01:19 +0530

    remove extra reference and api

--- NalaTest.API/Controllers/UserController.cs
+++ NalaTest.API/Controllers/UserController.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using NalaTest.Application.Interfaces;
 using NalaTest.Domain.Entity;
 using NalaTest.Domain.Entity.DTOs;
